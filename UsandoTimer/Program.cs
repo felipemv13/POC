b96995c0c2717e0d1f740238adb4c769b5b88bd6
FelipeC <felipe.jewish@gmail.com>
2017-07-16 02:57:40 -0300

    Criação de programa Timer gravando log.

---
+++ UsandoTimer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace UsandoTimer
+{
+    class TimerExample
+    {
+        static public void Tick(Object stateInfo)
+        {
+            var tick = $"Tick: {DateTime.Now.ToString("hh:mm:ss")}";
+            Console.WriteLine(tick);
+            RecordFile(tick);
+        }
+
+        static void Main()
+        {
+            TimerCallback callback = new TimerCallback(Tick);
+
+            Console.WriteLine("Creating timer: {0}\n",
+                               DateTime.Now.ToString("hh:mm:ss"));
+
+            // create a one minute timer tick
+            Timer stateTimer = new Timer(callback, null, 0, 60000);
+
+            string folder = @"C:\Users\Public\Log"; //nome do diretorio a ser criado
+
+            //Se o diretório não existir...
+            if (!Directory.Exists(folder))
+            {
+                //Criamos um com o nome folder
+                Directory.CreateDirectory(folder);
+            }
+
+            // loop here forever
+            for (;;)
+            {
+                // add a sleep for 100 mSec to reduce CPU usage
+                Thread.Sleep(100);
+            }
+        }
+
+        static void RecordFile(string text)
+        {
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"C:\Users\Public\Log\logBateria.txt", true))
+            {
+                file.WriteLine(text);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
