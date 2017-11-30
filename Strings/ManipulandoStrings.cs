bda06a8a224893dea9fb953011f8536ebd6ad38b
FelipeC <felipe.jewish@gmail.com>
2017-11-30 16:56:44 -0200

    evolução.

--- Strings/ManipulandoStrings.cs
+++ Strings/ManipulandoStrings.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+using System.Text;
 
 namespace Strings
 {
@@ -9,13 +11,13 @@ namespace Strings
         {
             string rua = null;
             string bairro = "Vila Sésamo";
-            DateTime dt = new DateTime(2017,10,13);
+            DateTime dt = new DateTime(2017, 10, 13);
 
             if (dt == default(DateTime))
-                return;
+                dt = DateTime.Now;
 
             var queryEndereco = $@"INSERT INTO [dbo].[TESTE] ([Rua], [Bairro], [DataCriacao])
-                        VALUES ('{rua ?? ""}', '{bairro ?? null}', '{dt == ? "" : {dt}');
+                        VALUES ('{rua ?? ""}', '{bairro ?? null}', '{dt}';
                         SELECT CAST(scope_identity() AS int)";
 
             string x = null;
@@ -24,7 +26,43 @@ namespace Strings
 
             var w = x ?? y ?? z;
 
+            double? valortotal = 10.90;
+
             var novoEnd = $"{queryEndereco}";
+
+            var queryString = "";
+            var queryField = new StringBuilder();
+            var queryInsertValue = new StringBuilder();
+            var id = new Guid();
+
+            Console.WriteLine($"Time: {DateTime.Now:yyyy-MM-dd HH:mm:ss}");
+
+            var formatDecimal = new CultureInfo("en-US");
+
+            queryField.Append("INSERT INTO [dbo].[Consumidor] ");
+            queryInsertValue.Append(" VALUES ");
+            queryField.Append("([id]");
+            queryInsertValue.Append($"('{Guid.NewGuid()}'");
+            queryField.Append(", [CpfCnpj] ");
+            queryInsertValue.Append(", '15232515880'");
+            queryField.Append(", [valortotalUs] ");
+            queryInsertValue.Append($", {valortotal?.ToString(formatDecimal)}");
+            valortotal = null;
+            queryField.Append(", [valortotalBr1] ");
+            queryInsertValue.Append($", {(valortotal != null ? valortotal?.ToString(formatDecimal) : 0.ToString())}");
+            queryField.Append(", [valortotalBr2] ");
+            queryInsertValue.Append($", {(valortotal != null ? valortotal?.ToString(formatDecimal) : Convert.ToString("0"))}");
+            queryField.Append(", [valortotalBr2] ");
+            queryInsertValue.Append($", {(valortotal != null ? valortotal?.ToString(formatDecimal) : "0")}");
+            queryField.AppendLine(")");
+            queryInsertValue.AppendLine(");");
+
+            queryInsertValue.AppendLine(" SELECT (scope_identity())");
+
+            queryString = queryField.ToString() + queryInsertValue.ToString();
+            queryField.Clear();
+            queryInsertValue.Clear();
+
         }
     }
 }
